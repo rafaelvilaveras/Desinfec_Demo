17e12099ccc51acf2fa0bf5736376b3aa9ea1676
rafaelvilaveras <rafaelvilaveras20@gmail.com>
2023-05-16 23:01:02 -0300

    teste

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -10,13 +10,13 @@ const Calendar = ({tDay, tMonth}) => {
         <div className='flex-row ca-container'>
             <div className='flex-column ca-hours'>
                 {new Array(13).fill(['', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18']).map((time, key)=>{
-                    return <span>{time[key]}</span>
+                    return <span className='ca-hours-content'>{time[key]}</span>
                 })}
             </div>
             <div className='flex-column ca-content'>
                 <div className='flex-row ca-header'>
                     {new Array(5).fill([tDay, tDay+1, tDay+2, tDay+3, tDay+4]).map((data, key)=>{
-                        return <span className={5 !== key+1 ? 'ca-header-border' : ''}>{data[key]}</span>
+                        return <span className={5 !== key+1 ? 'ca-header-border ca-header-content' : ''}>{data[key]}</span>
                     })}
                 </div>
                 <div className='ca-content'>
